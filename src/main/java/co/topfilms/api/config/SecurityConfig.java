5a97dd92b9199503118d05bb7e81b485f6940937
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-01-16 15:40:41 -0500

    added config for resource server

--- src/main/java/co/topfilms/api/config/SecurityConfig.java
+++ src/main/java/co/topfilms/api/config/SecurityConfig.java
@@ -1,8 +1,60 @@
 package co.topfilms.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
+    @Value("${topfilms.frontend.url}")
+    private String frontendUrl;
+
+    @Value("${spring.security.oauth2.resourceserver.jwk-set-uri}")
+    private String jwkUri;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfig()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth -> oauth
+                        .jwt(jwt -> jwt.jwkSetUri(jwkUri))
+                )
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfig() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedMethods(List.of("GET", "POST"));
+        config.setAllowedOrigins(List.of(frontendUrl));
+        config.setAllowedHeaders(List.of("*"));
+        config.setMaxAge(3600L);
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
+    public NimbusJwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(jwkUri).build();
+    }
 
 }
---
+++ src/main/java/co/topfilms/api/controllers/Testing.java
@@ -0,0 +1,38 @@
+package co.topfilms.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class Testing {
+
+    @GetMapping()
+    @CrossOrigin("http://localhost:3000")
+    public ResponseEntity<Test> test() {
+        return new ResponseEntity<>(new Test("Testing API!"), HttpStatus.OK);
+    }
+
+    public static class Test {
+        private String data;
+
+        public Test() {}
+
+        public Test(String data) {
+            this.data = data;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+    }
+
+}
