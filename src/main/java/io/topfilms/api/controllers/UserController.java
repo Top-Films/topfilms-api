8059c84404dd251cb594446a80e019464cb1f70d
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-04-02 19:27:44 -0400

    logging tests

--- src/main/java/io/topfilms/api/controllers/UserController.java
+++ src/main/java/io/topfilms/api/controllers/UserController.java
@@ -1,8 +1,8 @@
 package io.topfilms.api.controllers;
 
 import io.topfilms.api.entities.User;
-import io.topfilms.api.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+//import io.topfilms.api.repositories.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -13,17 +13,19 @@ import java.util.UUID;
 @Controller
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+//    @Autowired
+//    private UserRepository userRepository;
 
     @QueryMapping
     public List<User> users() {
-        return userRepository.findAll();
+//        return userRepository.findAll();
+        return null;
     }
 
     @QueryMapping
     public Optional<User> userById(UUID id) {
-        return userRepository.findById(id);
+//        return userRepository.findById(id);
+        return null;
     }
 
 }
